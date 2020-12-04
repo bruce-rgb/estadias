936674267e1d07e0e9c2a38562f3cd5586646c05
Bruce Robinson <3518111154@uth.edu.mx>
2020-12-04 13:03:00 -0600

    Se crean redirecciones personalizadas

--- resources/views/alumno.blade.php
+++ resources/views/alumno.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    {{--print_r($commerce)--}}
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,21 +13,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Home page
 Route::get('/', function () {
-    return view('auth.login');
+    //Si inició sesión redirigirlo a su pagina principal
+    if (Auth::check()) {
+        if (auth()->user()->perfil=='Alumno') {
+            return redirect('alumno');
+        }
+        if (auth()->user()->perfil=='Administrador de Sistema') {
+            return redirect('admin');
+        }
+        if (auth()->user()->perfil=='Director de Carrera') {
+            return redirect('director');
+        }
+        if (auth()->user()->perfil=='Profesor de Asignatura') {
+            return redirect('tutor');
+        }
+        if (auth()->user()->perfil=='Profesor de Tiempo completo') {
+            return redirect('tutor');
+        }
+        if (auth()->user()->perfil=='Biblioteca') {
+            return redirect('biblioteca');
+        }
+        if (auth()->user()->perfil=='Secretario Académico') {
+            return redirect('sa');
+        }
+    //si no a loguearse :'Y
+    }else{
+        return view('auth.login');
+    }
 });
 
+//Desactivar rutas de registro y verificación
 Auth::routes(['register'=>false,'verify'=>false]);
 
 //Ruta luego de login
 Route::get('/alumno', 'AlumnoController@index')->name('alumno')->middleware('auth');
 
+
 Route::get('/admin', 'AdminController@index')->name('admin')->middleware('auth');
+
 
 Route::get('/director', 'DirectorController@index')->name('director')->middleware('auth');
 
+
 Route::get('/tutor', 'TutorController@index')->name('tutor')->middleware('auth');
+
 
 Route::get('/biblioteca', 'BibliotecaController@index')->name('biblioteca')->middleware('auth');
 
+
 Route::get('/sa', 'SaController@index')->name('sa')->middleware('auth');
+
